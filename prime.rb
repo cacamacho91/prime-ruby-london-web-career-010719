76768813f57f516e5d67f04d9e8adf1ec1abf892
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-25 11:36:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,7 @@
 def prime?(number)
   is_prime = True
   for num in 2..number-1 do
-    num % 2 == 0
+    is_prime = False if num % 2 == 0
   end
+  is_prime
 end
-
-et Num from user
-get IsPrime = True
-for PFactor ranges from 2 to Num-1 do
-  begin block
-     if Num divisible by PFactor then set IsPrime = False
-  end block
-if IsPrime = True then display Num is prime
-else display Num is not prime
